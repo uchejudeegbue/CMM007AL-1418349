47506eeaddc4226808eeb11073e59ddd4b9a3d6a
1418349 <a@a.com>
2016-04-19 16:04:24 +0100

    Coding display to show my name

--- connect.php
+++ connect.php
@@ -10,5 +10,5 @@ $sql = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 if ($sql->connect_error) {
     die("Connection failed: " . $sql->connect_error);
 }
-echo "Connected successfully";
+
 ?>
