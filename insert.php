e3323263e011dd173cacf3347c1455cb79c61f51
1418349 <a@a.com>
2016-04-19 15:53:58 +0100

    CMM007

--- insert.php
+++ insert.php
@@ -3,21 +3,24 @@
 include("connect.php");
 
 if (empty($_POST['firstname'])||(empty($_POST['lastname']))){
-
+    echo "Both fields are required";
 }
     else{
-        echo "Both fields are required";
+
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+
+        $sql = "INSERT INTO testing (firstname, lastname) VALUES ($fisrtname, $lastname)";
+
+        if (mysqli_query($db,$sql)){
+            echo "Successful";
+        }
+        else {
+            echo "Error: " . $sql . "<br>" . mysqli_error ($db);
+        }
+
+
     }
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
 
-$sql = "INSERT INTO testing (firstname, lastname) VALUES ($fisrtname, $lastname)";
-
-       if (mysqli_query($db,$sql)){
-       echo "Successful";
-       }
-           else {
-          echo "Error: " . $sql . "<br>" . mysqli_error ($db);
-           }
 ?>
