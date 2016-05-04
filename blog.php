35eedb9d26725eff94128ce26bb427d91551c5f1
1418349 <a@a.com>
2016-05-04 10:43:27 +0100

    Assess Lab

---
+++ blog.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1418349
+ * Date: 04/05/2016
+ * Time: 10:42
+ */
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blog</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!--Start of header-->
+<header>
+
+    <h2>myBlog</h2>
+
+    <p>because the internet needs to know what I think</p>
+
+
+    <div class="nav">
+        <nav>
+            <ul>
+                <li><a href="blog.php">All Blog Items</a></li>
+                <li><a href="blog.php">Work items</a></li>
+                <li><a href="blog.php">University Items</a></li>
+                <li><a href="blog.php">Family Items</a></li>
+                <li><a href="add.php">Insert a Blog Item</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<!--End of header-->
+
+
+<!--Start of main-->
+<main>
+    <div class="article">
+        <p>Lorem ipsum doior sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugait nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia desertunt moilit anim id
+            est
+            laborum.</p>
+
+    </div>
+    <div class="logo">
+
+        <img id="logo" src="blog.png"/>
+    </div>
+
+</main>
+<!--End of main-->
+
+
+<!--Start of footer-->
+<footer>
+    <p>Designed by [Uche Jude Egbue], [2016]</p>
+</footer>
+<!--End of footer-->
+
+
+</body>
+</html>
